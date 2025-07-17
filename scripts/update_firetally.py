b4b0c1eaec9ead42851b20f4398ac358d06b68f9
Chris Waigl <chris.waigl@gmail.com>
2025-07-16 16:51:25 -0800

    fire tally getting it to work

--- scripts/update_firetally.py
+++ scripts/update_firetally.py
@@ -8,6 +8,7 @@
 from hnl_fire_data import firetallytools as ft
 from pathlib import Path
 import datetime as dt
+import pandas as pd
 import argparse
 
 # edit these as required
@@ -18,10 +19,13 @@ PROJDIR = Path().absolute().parent
 FIGDIR = PROJDIR / "figures"
 OUTDIR = PROJDIR / "data/working"
 REPORTDIR = Path().absolute().parents[1]  / 'data/AICC_reports'   # outside the repo directory - change if desired
+DOWNLOAD = True         # set to False to skip downloading new reports
 OVERWRITE = False       # overwrite files when downloading
 STARTDATE = '20250415'  # date to start looking for situation reports
 YEAR = 2025
 TODAY = dt.datetime.now().strftime("%Y%m%d")
+PLOT_OLD_YEARS = True  # if True, plot daily totals for previous years as well
+OLDDATAFP = PROJDIR / "resources/AICC_Daily_Stats_2004_to_Present.csv"
 
 def parse_arguments():
     """Parse arguments"""
@@ -29,6 +33,7 @@ def parse_arguments():
     parser.add_argument('-g', '--github_action', 
         help="run as github action",
         action="store_true")
+    return parser.parse_args()
 
 def main() -> None:
     args = parse_arguments()
@@ -36,23 +41,64 @@ def main() -> None:
         raise Exception("Not implemented yet")
     
     # step 1: Download new reports
-    REPORTDIR.mkdir(parents=True, exist_ok=True)
-    _ = ft.download_reports(startdatestr=STARTDATE,
-                               URLtemplate=URLTEMPLATE,
-                               fntemplate=FNTEMPLATE,
-                               outdir=REPORTDIR,
-                               overwrite=False)
+    if not DOWNLOAD:
+        print("DOWNLOAD is set to False, skipping download of reports.")
+        print(f"Set DOWNLOAD=True in {__file__} to download new reports.")
+        print(f"Using reports in {REPORTDIR}")
+    else:
+        print("DOWNLOAD is set to True, downloading new reports.")
+        print(f"Overwrite existing files is set to {OVERWRITE}")
+        print(f"Starting download from {STARTDATE} using URL template {URLTEMPLATE}")
+        REPORTDIR.mkdir(parents=True, exist_ok=True)
+        print(f"Downloading reports to {REPORTDIR}")
+        _ = ft.download_reports(startdatestr=STARTDATE,
+                                URLtemplate=URLTEMPLATE,
+                                fntemplate=FNTEMPLATE,
+                                outdir=REPORTDIR,
+                                overwrite=False)
     
     # step 2: assemble the dataframe from all reports and save it
+    OUTDIR.mkdir(parents=True, exist_ok=True)
     all_updates = ft.assemble_dataframe(REPORTDIR, FNTEMPLATE)
     all_updates.to_csv(OUTDIR / f"all_updates_{YEAR}_{TODAY}.csv")
-    # step 3: clean up, remove Rx and FA fires, add PSAs, save again
-    all_updates = all_updates.query("`Incident Type` != 'RX-Prescribed Fire'")
-    all_updates = all_updates.query("`Incident Type` != 'FA-False Alarm'")
-    all_updates_PSA = ft.add_psa(all_updates, projdir=PROJDIR)
-    all_updates_PSA.to_csv(OUTDIR / f"all_updates_PSA_{YEAR}_{TODAY}.csv")
-    # step 4: make daily tallies this year by PSA, by Zone, and by year
+    print(f"The all updates dataframe has been assembled with {len(all_updates)} records.")
 
+    # step 3: clean up, remove Rx and FA fires, add PSAs, save again
+    all_updates.dropna(subset=['Incident Type'], inplace=True) # remove rows with no incident type (Complexes)
+    all_updates = all_updates.query("`Incident Type` != 'RX-Prescribed Fire'")  # remove prescribed fires
+    all_updates = all_updates.query("`Incident Type` != 'FA-False Alarm'")  # remove false alarms
+    all_updates_PSA = ft.add_psa(all_updates, projdir=PROJDIR)
+    all_updates_PSA.to_csv(OUTDIR / f"all_updates_clean_PSA_{YEAR}_{TODAY}.csv")
+    
+    # step 4: make daily tallies this year by PSA, by Zone, and by year
+    dailyarea_by_PSA = ft.aggregate_by_day_region(all_updates_PSA, region='PSA')
+    dailyarea_by_PSA.to_csv(OUTDIR / f"dailyarea_PSA_{YEAR}_{TODAY}.csv")
+    dailyarea_by_Zone = ft.aggregate_by_day_region(all_updates_PSA, region='Zone')
+    dailyarea_by_Zone.to_csv(OUTDIR / f"dailyarea_Zone_{YEAR}_{TODAY}.csv")
+    dailyarea = dailyarea_by_Zone[['reportdate', 'Acres']].groupby('reportdate').sum()
+    dailyarea.to_csv(OUTDIR / f"dailyarea_total_{YEAR}_{TODAY}.csv")
+
+    # step 5: plot daily regional tallies for the current year
+    FIGDIR.mkdir(parents=True, exist_ok=True)
+    ft.plot_dailyarea_by_region(dailyarea_by_PSA, region='PSA', figdir=FIGDIR, plotday=TODAY)
+    ft.plot_dailyarea_by_region(dailyarea_by_Zone, region='Zone', figdir=FIGDIR, plotday=TODAY)
+
+    # step 6: plot daily totals for the current year and if desired, previous years
+    if PLOT_OLD_YEARS:
+        if OLDDATAFP.exists():
+            olddata = ft.load_old_data(OLDDATAFP)
+            dailyarea_by_Zone_reformatted = ft.reformat_newdata(dailyarea_by_Zone)
+            alldaily_totals = ft.combine_daily_totals(olddata, dailyarea_by_Zone_reformatted)
+            alldaily_totals.to_csv(OUTDIR / f"alldaily_totals_by_Zone_to_{YEAR}_{TODAY}.csv", index=False)
+            olddata_daily = ft.olddata_to_daily(olddata)
+            dailyarea['Year'] = dailyarea.index.year
+            dailyarea.reset_index(inplace=True)
+            all_data_daily = pd.concat([olddata_daily, dailyarea])
+            ft.plot_dailytotals_by_year(all_data_daily, olddata=True, annualthreshold=600000, figdir=FIGDIR, today=TODAY)
+            return
+        else:
+            print(f"Old data file {OLDDATAFP} not found. Skipping plotting of previous years.")
+    ft.plot_dailytotals_by_year(dailyarea, olddata=False, figdir=FIGDIR, today=TODAY)
 
 if __name__ == '__main__':
     main()
--- src/hnl_fire_data/firetallytools.py
+++ src/hnl_fire_data/firetallytools.py
@@ -25,13 +25,16 @@ GROUPINGS = {
         'PSA': ['reportdate', 'PSA_NAME', 'NAT_CODE'],
         'Zone': ['reportdate', 'Protecting Office', 'Protecting Office Label']
 }
+PLOTVAR_idx = {'PSA': 1, 'Zone': 1} # which column to plot for region by GROUPINGS list index
+PLOTSTARTDATE = '2025-05-14'  # start plotting from this date
+YEAR = 2025
 
 def download_reports(startdatestr: str, 
                      URLtemplate: str, 
                      fntemplate: str,
                      outdir: Path, 
                      overwrite: bool=False) -> str:
-    """Download situationrreports from AICC site"""
+    """Download situation reports from AICC site"""
     lastdate = None
     for item in pd.date_range(startdatestr,dt.datetime.now().strftime("%Y%m%d"), freq='d'):
         thedate = item.strftime('%Y%m%d')
@@ -71,6 +74,7 @@ def extract_zone(row: pd.Series) -> str:
         
 def assemble_dataframe(datadir:Path, 
                        fntemplate: str) -> pd.DataFrame:
+    """Construct this year's dataframe from downloaded files"""
     results = []
     for fp in datadir.glob(f'{fntemplate}*'):
         datestamp = fp.stem[-8:]
@@ -101,6 +105,7 @@ def gdf_from_df(updatesDF: pd.DataFrame) -> gp.GeoDataFrame:
 
 def add_psa(all_updatesDF: pd.DataFrame, 
             projdir: Path) -> gp.GeoDataFrame:
+    """Join all updates with PSA boundaries"""
     psa_GDF = get_psaGDF(projdir=projdir)
     all_updates_GDF = gdf_from_df(all_updatesDF)
     joined_GDF = gp.sjoin(all_updates_GDF, psa_GDF, predicate='within', how='inner')
@@ -109,6 +114,7 @@ def add_psa(all_updatesDF: pd.DataFrame,
 
 def aggregate_by_day_region(updatesDF: pd.DataFrame,
                             region: str) -> pd.DataFrame:
+    """Aggregate daily burned area by region. Region is one of GROUPINGS keys."""
     try:
         dailyarea_agg = updatesDF[
             GROUPINGS[region] + ['Acres']].groupby(GROUPINGS[region]).sum().reset_index()
@@ -117,3 +123,114 @@ def aggregate_by_day_region(updatesDF: pd.DataFrame,
     dailyarea_agg['Acres'] = dailyarea_agg['Acres'].replace(0, pd.NA)
     dailyarea_agg.dropna(inplace=True)
     return dailyarea_agg
+
+def load_old_data(olddatafp: Path) -> pd.DataFrame:
+    """Load old data from the file"""
+    if not olddatafp.exists():
+        raise FileNotFoundError(f"Old data file {olddatafp} does not exist.")
+    olddata = pd.read_csv(olddatafp, skiprows=2)
+    dropcols = [col for col in olddata.columns if col.startswith('Unname')] 
+    olddata.drop(columns=dropcols+['ID'], inplace=True)
+    intcols = list(olddata.columns.difference(['TotalAcres', 'ProtectionUnit', 'SitReportDate']))
+    for col in intcols:
+        if olddata[col].isna().any():
+            olddata.drop(columns=[col], inplace=True)
+        else:
+            olddata[col] = olddata[col].astype(int)
+    olddata.rename(columns={'FireSeason': 'Year'}, inplace=True)
+    olddata['reportdate'] = pd.to_datetime(olddata[['Year', 'Month', 'Day']])
+    olddata.drop(columns=['SitReportDate'], inplace=True)
+    olddata.rename({'TotalAcres': 'Acres'}, axis=1, inplace=True)
+    return olddata
+
+def olddata_to_daily(olddata: pd.DataFrame) -> pd.DataFrame:
+    """Convert old data to daily format"""
+    olddata.drop(columns=['Month', 'Day', 'TotalFires', 'ProtectionUnit'], inplace=True)
+    olddata = olddata.groupby(['reportdate', 'Year']).sum().reset_index()
+    olddata.rename({'TotalAcres': 'Acres'}, axis=1, inplace=True)
+    return olddata
+
+def reformat_newdata(dailyareabyZoneDF: pd.DataFrame) -> pd.DataFrame:
+    """Reformat new daily data to match old data format"""
+    dailyareabyZoneDF['Year'] = dailyareabyZoneDF.reportdate.dt.year
+    dailyareabyZoneDF['Month'] = dailyareabyZoneDF.reportdate.dt.month
+    dailyareabyZoneDF['Day'] = dailyareabyZoneDF.reportdate.dt.day
+    dailyareabyZoneDF.rename(columns={'Protecting Office Label': 'ProtectionUnit'}, inplace=True)
+    dailyareabyZoneDF.drop(columns=['Protecting Office'], inplace=True)
+    return dailyareabyZoneDF 
+
+def combine_daily_totals(olddata: pd.DataFrame,
+                        dailyarea: pd.DataFrame) -> pd.DataFrame:
+    """Combine old and new daily totals"""
+    combined = pd.concat([olddata, dailyarea], ignore_index=True)
+    return combined
+
+def plot_dailyarea_by_region(dailyareaDF: pd.DataFrame,
+                             region: str,
+                             figdir: Path=None,
+                             plotday: str=None,
+                             savefig=True) -> None:
+    """Plot daily burned area by region"""
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    import seaborn as sns
+    import colorcet as cc
+    mpl.rcParams['figure.dpi'] = 150
+    sns.set_theme('paper')
+    sns.set_style('whitegrid')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(data=dailyareaDF.loc[PLOTSTARTDATE:], x='reportdate', y='Acres', 
+                hue=GROUPINGS[region][PLOTVAR_idx[region]], ax=ax, palette=sns.color_palette(cc.glasbey_dark))
+    plt.title(f"{YEAR} daily area burned by {GROUPINGS[region][PLOTVAR_idx[region]]} (from AICC Situation Reports)")
+    plt.xlabel("Date of situation report")
+    ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+    plt.tight_layout()
+    if savefig:
+        outfp = figdir / f"dailyarea_{region}_{plotday}.png"
+        fig.savefig(outfp, bbox_inches='tight')
+        print(f"Saved figure to {outfp}")
+        plt.close()
+    else:
+        plt.show()
+    return None 
+
+def plot_dailytotals_by_year(dailyareaDF: pd.DataFrame,
+                             olddata: bool=True,
+                             annualthreshold: int=None,
+                             figdir: Path=None,
+                             today: str=None,
+                             savefig=True) -> None:
+    """Plot daily burned area totals by year"""
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    import seaborn as sns
+    import colorcet as cc
+    mpl.rcParams['figure.dpi'] = 150
+    sns.set_theme('paper')
+    sns.set_style('whitegrid')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    if olddata:
+        dailyareaDF['reportdate'] = dailyareaDF.reportdate.map(lambda t: t.replace(year=YEAR))
+        if annualthreshold:
+            big_fires_years = dailyareaDF[dailyareaDF.Acres > annualthreshold].sort_values(
+                'Acres', ascending=False).drop_duplicates('Year').Year.to_list()
+            dailyareaDF = dailyareaDF[dailyareaDF.Year.isin(big_fires_years)]
+        sns.lineplot(data=dailyareaDF, x='reportdate', y='Acres', 
+                    hue='Year', ax=ax, palette=sns.color_palette(cc.glasbey))
+        ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%m-%d'))
+        plt.title("Daily area burned by Fire Season")
+    else:
+        sns.lineplot(data=dailyareaDF.loc[PLOTSTARTDATE:], x='reportdate', y='Acres', 
+                    ax=ax)
+        plt.title(f"{YEAR} daily area burned (from AICC Situation Reports)")
+    ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+    ax.set_xlabel("Date")
+    plt.tight_layout()
+    if savefig:
+        outfp = figdir / f"dailytotals_by_year_{'olddata' if olddata else '2025only'}_{today}.png"
+        fig.savefig(outfp, bbox_inches='tight')
+        print(f"Saved figure to {outfp}")
+        plt.close()
+    else:
+        plt.show()
+    return None
